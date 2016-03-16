0beb78a5da3ce6cc377e4350db449fb7c7b40ad8
minkcv <mink.cv@gmail.com>
2016-03-15 22:27:00 -0500

    fix ordering and relative path issue

--- setup.sh
+++ setup.sh
@@ -2,15 +2,15 @@
 
 ln -s ~/.colorfulconfig/.xinitrc ~/.xinitrc
 ln -s ~/.colorfulconfig/.Xresources ~/.Xresources
-ln -s ~/.colorfulconfig/vim/vimrc ~/.vimrc
 ln -s ~/.colorfulconfig/i3config ~/.i3/config
 
 mkdir ~/.vim
 ln -s ~/.colorfulconfig/vim/colors ~/.vim/colors
+ln -s ~/.colorfulconfig/vim/vimrc ~/.vimrc
 
 mkdir ~/.tmux
 ln ~/.colorfulconfig/.tmux.config ~/.tmux/.tmux.conf
 
 echo "alias tmux='tmux -2'" >> .bashrc
 
-echo "source .colorfulconfig/base16-default.dark.sh" >> .bashrc
+echo "source .colorfulconfig/base16-default.dark.sh" >> ~/.bashrc
