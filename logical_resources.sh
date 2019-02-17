620c28e13ccb7ff04957a07edfaae0fef44dd170
Will Smith <minkcv@gmail.com>
2019-02-17 16:11:09 -0700

    add fictional login header

---
+++ logical_resources.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+clear
+v=$(uname -r)
+cat << EOF
+       aeab7ca1bec0c488       
+      c10e34d6b531ecb867      
+     6482546a14232f73b342     
+    634572          06c5f9    		Logical Resources Inc.
+   d3c2a0            133bc9   
+  7398b8              7fe232  		Kernel Release $v
+ 2a8ece                ac9559 
+ 915a4c                699db7 
+  ca5d67              b49e0c  
+   215bc2            adc660   
+    f0e885          b43dbf    
+     66f7d69abba59ea97120     
+      fcb4f06c41f31b3bea      
+       966c81418171e75c       
+
+EOF
